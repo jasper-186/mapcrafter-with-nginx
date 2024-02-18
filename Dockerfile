2ab80e68766b4de82d9ebbfb83739bb1feef2df6
Joe Echtenkamp <github@echtenkamp.com>
2024-02-18 15:35:37 -0600

    the start sh handles the default stuff

--- Dockerfile
+++ Dockerfile
@@ -7,8 +7,6 @@ RUN apk add nginx
 # Copy the Nginx config
 COPY /defaults/nginx/default-site.conf /etc/nginx/sites-available/default
 
-RUN mkdir -p /defaults/mapcrafter
-COPY ./defaults/mapcrafter/* /defaults/mapcrafter/
 COPY  init.sh /init.sh
 
 # Expose the port for access
--- init.sh
+++ init.sh
@@ -1,30 +1,10 @@
 #!/bin/sh
 #check for defaults, and copy them in
 
-echo "Checking for favicon"
-if [ ! -f "/config/favicon.png" ]; then
-    cp "/defaults/mapcrafter/favicon.png" "/config/favicon.png"
-fi
-
-echo "Checking for title.txt"
-if [ ! -f "/config/title.txt" ]; then
-    cp "/defaults/mapcrafter/title.txt" "/config/title.txt"
-fi
-
-echo "Checking for render.conf"
-if [ ! -f "/config/render.conf" ]; then
-    cp "/defaults/mapcrafter/render.conf" "/config/render.conf"
-fi
-
-echo "Checking for mapcrafter-cron"
-if [ ! -f "/config/mapcrafter-cron" ]; then
-    cp "/defaults/mapcrafter/mapcrafter-cron" "/config/mapcrafter-cron"
-fi
-
 echo "Begining rendering of the world?"
 # Generate the world
 sh /opt/start.sh
 
 echo "Starting nginx server"
 # start the webserver
-/usr/sbin/nginx -g "daemon off"
+/usr/sbin/nginx -g "daemon off;"
