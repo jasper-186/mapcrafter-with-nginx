5333b6a9799205e676ea4462ecbf56a369e77bd9
Joe Echtenkamp <github@echtenkamp.com>
2024-02-18 15:40:03 -0600

    add folder for pid file

--- init.sh
+++ init.sh
@@ -5,6 +5,9 @@ echo "Begining rendering of the world?"
 # Generate the world
 sh /opt/start.sh
 
+#nginx needs a pid file here
+RUN mkdir -p /run/nginx
+
 echo "Starting nginx server"
 # start the webserver
 /usr/sbin/nginx -g "daemon off;"
