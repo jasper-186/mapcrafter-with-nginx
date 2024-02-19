2edc345ed18068059f95586b89a5906413b20fd8
Joe Echtenkamp <github@echtenkamp.com>
2024-02-18 22:47:39 -0600

    truncate the log file to clear old entries

--- init.sh
+++ init.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 #check for defaults, and copy them in
 
+# Reset the Log file
+truncate -s 0 /config/mapcrafter.log
+
 # Generate the world
 echo "Begining rendering of the world?"
 sh /opt/start.sh 
@@ -12,5 +15,5 @@ echo "Starting nginx server"
 /usr/sbin/nginx #-g "daemon off;"
 
 echo "Tailing the log file"
-#follow the generator log so we can track progress of the generation
+# follow the generator log so we can track progress of the generation
 tail -f /config/mapcrafter.log
